41c0b40d8dd270fb08afa00a6da79bf80a868ccc
Mateusz Borowiecki <borowieckimateusz1@gmail.com>
2024-01-06 21:25:45 +0100

    removed protected from about screen

--- src/core/router/routes.tsx
+++ src/core/router/routes.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { AboutScreen } from 'feature/about/presentation';
-import { LoginScreen } from 'feature/auth/presentation';
-=======
 import { AdminPanelScreen } from 'feature/adminPanel/presentation';
 import { LoginScreen, RegisterScreen } from 'feature/auth/presentation';
->>>>>>> 53df5583ba0db6cfba244cd42c1a802a8f93ec09
 import { DashboardScreen } from 'feature/dashboard/presentation';
 import { NotFoundScreen } from 'feature/notFound/presentation/notFound.screen';
 import { createBrowserRouter } from 'react-router-dom';
@@ -29,18 +25,14 @@ export const browserRouter = createBrowserRouter([
     Component: RegisterScreen
   },
   {
+    path: '/about',
+    Component: AboutScreen
+  },
+  {
     path: '/admin',
     Component: () => (
       <ProtectedRoute>
         <AdminPanelScreen />
-      </ProtectedRoute>
-    )
-  },
-  {
-    path: '/about',
-    Component: () => (
-      <ProtectedRoute>
-        <AboutScreen />
       </ProtectedRoute>
     )
   },
